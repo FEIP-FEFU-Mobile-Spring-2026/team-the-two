b2cf33fe30d0418c5f89e7168a261d0916de94aa
nariia-ang <lextafour@gmail.com>
2026-05-23 15:55:06 +1000

    Добавлен экран ProductDetailsActivity; кнопка на главной странице, чтобы попасть на новый экран; несколько черновых компонентов для нового экрана.

--- app/src/main/java/com/example/myshop/ProductDetailsActivity.kt
+++ app/src/main/java/com/example/myshop/ProductDetailsActivity.kt
@@ -1,31 +1,20 @@
 package com.example.myshop
 
 import android.os.Bundle
-import android.widget.Button /// добавление Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import android.content.Intent /// добавление Intent
 
-class MainActivity : AppCompatActivity() {
+class ProductDetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_product_details)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        /// переход к активности просмотра товара
-        val buttonToProductDetails: Button = findViewById<Button>(R.id.buttonToProductDetails)
-
-        buttonToProductDetails.setOnClickListener {
-            val intent = Intent(this, ProductDetailsActivity::class.java)
-            startActivity(intent)
-        }
-        ///
     }
 }
