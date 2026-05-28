b56c9097ea10f3e489b10351d16133ff8866bd40
nariia-ang <lextafour@gmail.com>
2026-05-28 18:52:07 +1000

    Catalog screen and necessary elements added, misspelling fixed

--- app/src/main/java/com/example/myshop/CatalogActivity.kt
+++ app/src/main/java/com/example/myshop/CatalogActivity.kt
@@ -17,13 +17,13 @@ class CatalogActivity: AppCompatActivity() {
     private lateinit var recyclerView: RecyclerView
     private lateinit var tabLayout: TabLayout
 
-    override fun onCreate(savedInstanceState: Bundle?){
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_catalog)
 
         val repository = ProductsRepository(this)
-        viewModel = ViewModelProvider(this, object: ViewModelProvider.Factory {
-            override fun <T: androidx.lifecycle.ViewModel> create(modelClass: Class<T>): T {
+        viewModel = ViewModelProvider(this, object : ViewModelProvider.Factory {
+            override fun <T : androidx.lifecycle.ViewModel> create(modelClass: Class<T>): T {
                 return CatalogViewModel(repository) as T
             }
         })[CatalogViewModel::class.java]
@@ -40,18 +40,18 @@ class CatalogActivity: AppCompatActivity() {
         }
 
         tabLayout = findViewById(R.id.tabLayout)
-        viewModel.categories.observe(this) {categories ->
+        viewModel.categories.observe(this) { categories ->
             categories.forEach { categoryName ->
                 tabLayout.addTab(tabLayout.newTab().setText(categoryName))
             }
         }
 
-        tabLayout.addOnTabSelectedListener(object :
-        TabLayout.OnTabSelectedListener {
-            override fun onTabListener(tab: TabLayout.Tab) {
+        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab) {
                 val category = tab.text.toString()
                 viewModel.selectCategory(category)
             }
+
             override fun onTabUnselected(tab: TabLayout.Tab) {}
             override fun onTabReselected(tab: TabLayout.Tab) {}
         })
